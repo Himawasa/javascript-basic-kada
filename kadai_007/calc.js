355f901be41848a708a20e6af401179859ad5ec0
Hiroshi.I <himawasa@gmail.com>
2024-11-16 18:38:17 +0900

    課題007初回提出

---
+++ kadai_007/calc.js
@@ -0,0 +1,21 @@
+let num = 3;
+// let num = 5;
+// let num = 6;
+// let num = 10;
+// let num = 15;
+
+
+
+if (num % 3 === 0 && num % 5 === 0) {
+    console.log("3と5の倍数です");
+  } else if (num % 3 === 0) {
+    console.log("3の倍数です");
+  } else if (num % 5 === 0) {
+    console.log("5の倍数です");
+  } else {
+    console.log(num);
+  }
+
+
+
+  
